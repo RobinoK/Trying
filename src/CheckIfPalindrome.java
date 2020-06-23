f86afba89025cd2f2ae6b5f410c99ac8d35ba0b8
RobinoK <robajz007@gmail.com>
2020-06-23 12:07:54 +0200

    Final version

--- src/CheckIfPalindrome.java
+++ src/CheckIfPalindrome.java
@@ -6,25 +6,19 @@ public class CheckIfPalindrome {
         System.out.println("Zadej slovo: ");
         String slovo = sc.nextLine();
         String pom = "";
-        char []pole = new char[slovo.length()];
         for(int i = slovo.length()-1; i >= 0 ; i--){
             char x = slovo.charAt(i);
-            pole[i] = x;
-            pom = String.valueOf(pole[i]) + pom;
-            System.out.println("Je slovo: "+x);
-            if(pom.equals(slovo)){
-                System.out.println("Je palindrom");
+            pom = pom + x;
+
 
             }
-            else {
-                System.out.println("neni palindrom");
-            }
-            }
+        if(pom.equals(slovo)){
+            System.out.println("Je palindrom");
 
-
-
-
-
+        }
+        else {
+            System.out.println("neni palindrom");
+        }
 
     }
 }
